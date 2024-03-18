785f8e56eacfeaf5978c25ab8377f9e4a373a4e6
DickLan <pokemaster556610@gmail.com>
2024-03-18 15:00:15 +0800

    auth status save in localStorage, keep status after refresh

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -14,41 +14,51 @@ export function AuthProvider({ children }) {
   // 登入與登出的方法
  const login = (user) => {
   setIsAuthenticated(true)
-  localStorage.setItem('currentUser', user)
+  setCurrentUser(user);
+  localStorage.setItem('currentUser', JSON.stringify(user));
   
  }
   const logout = () => {
     setIsAuthenticated(false)
+    setCurrentUser(null);
     localStorage.removeItem('currentUser')
     localStorage.removeItem('token')
   };
-
-
-
-
+  // useEffect 在元件首次渲染時執行，而F5重整頁面時，元件也會重新渲染，所以會再次調用這個 useEffect，然後從 LS 拿出使用者資訊，繼而恢復登入狀態
   useEffect(() => {
-    // useEffect 不能直接改成 async 函數
-    // 所以要像下面這樣寫
+    const storedUser = localStorage.getItem('currentUser');
+    if (storedUser) {
+      const user = JSON.parse(storedUser);
+      setCurrentUser(user);
+      setIsAuthenticated(true);
+    }
+  }, []);
+
+
+
+  // useEffect(() => {
+  //   // useEffect 不能直接改成 async 函數
+  //   // 所以要像下面這樣寫
     
-  const fetchData = async () => {
-    const response = await apiHelper.get("/fetchCurrentUser");
-    if (response.data) {
-    setCurrentUser(response.data);
-    setIsAuthenticated(true);
-  } else {
-    setIsAuthenticated(false);
-  }
-    // console.log("response.data:", response.data); 
-  };
-  if (isAuthenticated){
-    fetchData();
-  }
-  // 在 useEffect 裏面看到的數據，仍是初始值
-    // console.log("pre currentUser",currentUser)
-    // fetchData()
-    // console.log("after fetch currentUser",currentUser)
+  // const fetchData = async () => {
+  //   const response = await apiHelper.get("/fetchCurrentUser");
+  //   if (response.data) {
+  //   setCurrentUser(response.data);
+  //   setIsAuthenticated(true);
+  // } else {
+  //   setIsAuthenticated(false);
+  // }
+  //   // console.log("response.data:", response.data); 
+  // };
+  // if (isAuthenticated){
+  //   fetchData();
+  // }
+  // // 在 useEffect 裏面看到的數據，仍是初始值
+  //   // console.log("pre currentUser",currentUser)
+  //   // fetchData()
+  //   // console.log("after fetch currentUser",currentUser)
     
-  }, [isAuthenticated]); 
+  // }, [isAuthenticated]); 
 
   // 在元件的主體中才會看到更新
   console.log("currentUser in component body:", currentUser);
